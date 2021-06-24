19a1edcbcb5c74bac98077621832eac46f8d0ce5
daniel <danielsoledad@gmail.com>
2021-06-24 03:07:02 -0700

    added post and image ability

--- components/Feed/components/InputBox.js
+++ components/Feed/components/InputBox.js
@@ -2,20 +2,22 @@ import { useSession } from "next-auth/client";
 import Image from "next/image";
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
-import { useRef } from "react";
-import { db } from "../../../firebase";
+import { useRef, useState } from "react";
+import { db, storage } from "../../../firebase";
 import firebase from "firebase";
 
 const InputBox = () => {
     const [session] = useSession();
-	const inputRef = useRef(null)
+	const inputRef = useRef(null);
+	const fileRef = useRef(null);
+	const [imagePost, setImagePost] = useState(null)
 
-    const sendPost = (e) =>{
+    const sendPost = async (e) =>{
         e.preventDefault()
 
 		if(!inputRef.current.value) return
 
-		db.collection('post').add({
+		const doc = await db.collection('post').add({
 			message: inputRef.current.value,
 			name: session.user.name,
 			email: session.user.email,
@@ -24,9 +26,58 @@ const InputBox = () => {
 
 		})
 
+		if (imagePost) {
+        	const uploadTask = storage
+				.ref(`posts/${doc.id}`)
+				.putString(imagePost, "data_url");
+
+        	removeImage();
+
+        	uploadTask.on(
+            "state_changed",
+            null,
+            (error) => {
+              // ERROR function
+            	console.log(error);
+            },
+            () => {
+              // COMPLETE function
+            	storage
+                .ref("posts")
+                .child(doc.id)
+                .getDownloadURL()
+                .then((url) => {
+                	db.collection("posts").doc(doc.id).set(
+                    {
+                    	postImage: url,
+                    },
+                    { merge: true }
+                	);
+                });
+            }
+          );
+        }
+
 		inputRef.current.value = ""
 
     }
+
+	const addImage = (e) =>{
+		e.preventDefault()
+
+		const reader = new FileReader();
+
+		if(e.target.files[0]){
+			reader.readAsDataURL(e.target.files[0])
+		}
+
+		reader.onload = (readerEvent) => setImagePost(readerEvent.target.result)
+
+	}
+
+	const removeImage = () =>{
+		setImagePost(null)
+	}
 
     return (
 		<div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
@@ -48,6 +99,21 @@ const InputBox = () => {
 					/>
 					<button hidden onClick={sendPost}></button>
 				</form>
+				{imagePost && (
+					<div 
+						onClick={removeImage}
+						className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer">
+						{
+							// eslint-disable-next-line @next/next/no-img-element
+							<img
+								src={imagePost}
+								className="h-10 object-contain"
+								alt="example"
+							/>
+						}
+						<p className="text-xs text-red-500 text-center">Remove</p>
+					</div>
+				)}
 			</div>
 
 			<div className="flex justify-evenly p-3 border-t">
@@ -58,11 +124,19 @@ const InputBox = () => {
 					</p>
 				</div>
 
-				<div className="inputIcon">
+				<div
+					onClick={() => fileRef.current.click()}
+					className="inputIcon">
 					<CameraIcon className="h-7 text-green-400" />
 					<p className="text-xs sm:text-sm xl:text-base">
 						Photo/Video
 					</p>
+					<input
+						type="file"
+						hidden
+						onChange={addImage}
+						ref={fileRef}
+					/>
 				</div>
 
 				<div className="inputIcon">
